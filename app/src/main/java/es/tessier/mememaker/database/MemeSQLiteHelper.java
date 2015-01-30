e1bf470b06727e1bb1388f6034874a2bca8acbb5
David <david.hmhernandez@gmail.com>
2015-01-30 13:17:19 +0100

    Actividad SQLite Terminada

--- app/src/main/java/es/tessier/mememaker/database/MemeSQLiteHelper.java
+++ app/src/main/java/es/tessier/mememaker/database/MemeSQLiteHelper.java
@@ -1,12 +1,64 @@
 package es.tessier.mememaker.database;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import java.sql.SQLException;
+
 /**
  * Created by Evan Anger on 8/17/14.
  */
-public class MemeSQLiteHelper {
+public class MemeSQLiteHelper extends SQLiteOpenHelper{
+
+    public final static String DB_NAME ="memes.db";
+    public final static int DB_VERSION = 1;
+    public static final String TAG = MemeSQLiteHelper.class.getName();
+
+    public MemeSQLiteHelper(Context context){
+
+        super(context,DB_NAME,null,DB_VERSION);
+    }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_TABLE_MEMES);
+        db.execSQL(CREATE_TABLE_ANNOTATIONS);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
 
     //Meme Table functionality
+    static final String MEMES_TABLE ="MEMES";
+    static final String COLUMN_MEMES_ASSET ="asset";
+    static final String COLUMN_MEMES_NAME ="name";
+    static final String COLUMN_MEMES_ID ="_id";
+
+    static final String CREATE_TABLE_MEMES = "CREATE TABLE "+MEMES_TABLE + " ( "+COLUMN_MEMES_ID + "INTEGER PRIMARY KEY AUTOINCREMENT "+ COLUMN_MEMES_ASSET + " TEX NOT NULL," + COLUMN_MEMES_NAME + " TEXT NOT NULL );";
+
 
     //Meme Table Annotations functionality
+    public static final String ANNOTATIONS_TABLE = "ANNOTATIONS";
+    public static final String COLUMN_ANNOTATIONS_ID = "_id";
+    public static final String COLUMN_ANNOTATIONS_TITLE = "title";
+    public static final String COLUMN_ANNOTATIONS_X = "x";
+    public static final String COLUMN_ANNOTATIONS_Y = "y";
+    public static final String COLUMN_ANNOTATIONS_COLOR = "color";
+    public static final String COLUMN_FOREIGN_KEY_MEME = "fk_meme_id";
 
+    static final String CREATE_TABLE_ANNOTATIONS = "CREATE TABLE " + ANNOTATIONS_TABLE + " ( "+
+            COLUMN_ANNOTATIONS_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+            COLUMN_ANNOTATIONS_TITLE + " TEXT NOT NULL," +
+            COLUMN_ANNOTATIONS_X + " INTEGER NOT NULL," +
+            COLUMN_ANNOTATIONS_Y + " INTEGER NOT NULL," +
+            COLUMN_ANNOTATIONS_COLOR + " INTEGER NOT NULL, " +
+            COLUMN_FOREIGN_KEY_MEME + " INTEGER NOT NULL, " +
+            "FOREIGN KEY (" + COLUMN_FOREIGN_KEY_MEME + ") " +
+            " REFERENCES MEME("+COLUMN_MEMES_ID +") );";
 }
